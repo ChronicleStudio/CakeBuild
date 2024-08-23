059d9a0a52f7f27b24af85bbf1db588ad9c106da
JonathanArendt <arendtjda@gmail.com>
2024-08-23 18:38:08 -0500

    Updated LaunchSettings, ModInfo, and CSProj for all packages, renamed solution, added custom template.

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -1,5 +1,3 @@
-using System;
-using System.IO;
 using Cake.Common;
 using Cake.Common.IO;
 using Cake.Common.Tools.DotNet;
@@ -10,119 +8,121 @@ using Cake.Frosting;
 using Cake.Json;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
 using Vintagestory.API.Common;
 
 public static class Program
 {
-    public static int Main(string[] args)
-    {
-        return new CakeHost()
-            .UseContext<BuildContext>()
-            .Run(args);
-    }
+	public static int Main(string[] args)
+	{
+		return new CakeHost()
+			.UseContext<BuildContext>()
+			.Run(args);
+	}
 }
 
 public class BuildContext : FrostingContext
 {
-    public static readonly string[] ProjectNames = new string[] { "sanctuaries", "shackles", "snitches" };
-    public string BuildConfiguration { get; set; }
-    public string[] Versions { get; }
-    public string[] Names { get; }
-    public bool SkipJsonValidation { get; set; }
+	public static readonly string[] ProjectNames = new string[] { "sanctuaries", "shackles", "snitches", "groups", "Modular_Items" };
+	public string BuildConfiguration { get; set; }
+	public string[] Versions { get; }
+	public string[] Names { get; }
+	public bool SkipJsonValidation { get; set; }
 
-    public BuildContext(ICakeContext context)
-        : base(context)
-    {
-        Versions = new string[ProjectNames.Length];
-        Names = new string[ProjectNames.Length];
-        for (int i = 0; i < ProjectNames.Length; i++)
-        {
-            BuildConfiguration = context.Argument("configuration", "Release");
-            SkipJsonValidation = context.Argument("skipJsonValidation", false);
-            var modInfo = context.DeserializeJsonFromFile<ModInfo>($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/modinfo.json");
-            Versions[i] = modInfo.Version;
-            Names[i] = modInfo.ModID;
-        }
-    }
+	public BuildContext(ICakeContext context)
+		: base(context)
+	{
+		Versions = new string[ProjectNames.Length];
+		Names = new string[ProjectNames.Length];
+		for (int i = 0; i < ProjectNames.Length; i++)
+		{
+			BuildConfiguration = context.Argument("configuration", "Release");
+			SkipJsonValidation = context.Argument("skipJsonValidation", false);
+			var modInfo = context.DeserializeJsonFromFile<ModInfo>($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/modinfo.json");
+			Versions[i] = modInfo.Version;
+			Names[i] = modInfo.ModID;
+		}
+	}
 }
 
 [TaskName("ValidateJson")]
 public sealed class ValidateJsonTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context)
-    {
-        for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
-        {
-            if (context.SkipJsonValidation)
-            {
-                return;
-            }
-            var jsonFiles = context.GetFiles($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/assets/**/*.json");
-            foreach (var file in jsonFiles)
-            {
-                try
-                {
-                    var json = File.ReadAllText(file.FullPath);
-                    JToken.Parse(json);
-                }
-                catch (JsonException ex)
-                {
-                    throw new Exception($"Validation failed for JSON file: {file.FullPath}{Environment.NewLine}{ex.Message}", ex);
-                }
-            }
-        }
-    }
+	public override void Run(BuildContext context)
+	{
+		for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
+		{
+			if (context.SkipJsonValidation)
+			{
+				return;
+			}
+			var jsonFiles = context.GetFiles($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/assets/**/*.json");
+			foreach (var file in jsonFiles)
+			{
+				try
+				{
+					var json = File.ReadAllText(file.FullPath);
+					JToken.Parse(json);
+				}
+				catch (JsonException ex)
+				{
+					throw new Exception($"Validation failed for JSON file: {file.FullPath}{Environment.NewLine}{ex.Message}", ex);
+				}
+			}
+		}
+	}
 }
 
 [TaskName("Build")]
 [IsDependentOn(typeof(ValidateJsonTask))]
 public sealed class BuildTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context)
-    {
-        for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
-        {
-            context.DotNetClean($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}.csproj",
-                new DotNetCleanSettings
-                {
-                    Configuration = context.BuildConfiguration
-                });
+	public override void Run(BuildContext context)
+	{
+		for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
+		{
+			context.DotNetClean($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}.csproj",
+				new DotNetCleanSettings
+				{
+					Configuration = context.BuildConfiguration
+				});
 
 
-            context.DotNetPublish($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}.csproj",
-                new DotNetPublishSettings
-                {
-                    Configuration = context.BuildConfiguration
-                });
-        }
-    }
+			context.DotNetPublish($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}.csproj",
+				new DotNetPublishSettings
+				{
+					Configuration = context.BuildConfiguration
+				});
+		}
+	}
 }
 
 [TaskName("Package")]
 [IsDependentOn(typeof(BuildTask))]
 public sealed class PackageTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context)
-    {
-        context.EnsureDirectoryExists("../../bin/ZipStaging");
-        context.CleanDirectory("../../bin/ZipStaging");
-        context.EnsureDirectoryExists("../../bin/Releases");
-        context.CleanDirectory("../../bin/Releases");
-        for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
-        {
-            context.EnsureDirectoryExists($"../../bin/ZipStaging/{context.Names[i]}");
-            context.CopyFiles($"../../bin/{context.BuildConfiguration}/Mods/{BuildContext.ProjectNames[i]}/publish/*", $"../../bin/ZipStaging/{context.Names[i]}");
-            context.CopyDirectory($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/assets", $"../../bin/ZipStaging/{context.Names[i]}/assets");
-            context.CopyFile($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/modinfo.json", $"../../bin/ZipStaging/{context.Names[i]}/modinfo.json");
-            context.Zip($"../../bin/ZipStaging/{context.Names[i]}", $"../../bin/Releases/{context.Names[i]}_{context.Versions[i]}.zip");
-        }
-        DeleteDirectorySettings DDS = new()
-        {
-            Recursive = true
-        };
-        context.DeleteDirectory($"../../bin/ZipStaging/", DDS);
-        context.DeleteDirectory($"../../bin/Release", DDS);
-    }
+	public override void Run(BuildContext context)
+	{
+		context.EnsureDirectoryExists("../../bin/ZipStaging");
+		context.CleanDirectory("../../bin/ZipStaging");
+		context.EnsureDirectoryExists("../../bin/Releases");
+		context.CleanDirectory("../../bin/Releases");
+		for (int i = 0; i < BuildContext.ProjectNames.Length; i++)
+		{
+			context.EnsureDirectoryExists($"../../bin/ZipStaging/{context.Names[i]}");
+			context.CopyFiles($"../../bin/{context.BuildConfiguration}/Mods/{BuildContext.ProjectNames[i]}/publish/*", $"../../bin/ZipStaging/{context.Names[i]}");
+			context.CopyDirectory($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/assets", $"../../bin/ZipStaging/{context.Names[i]}/assets");
+			context.CopyFile($"../../{BuildContext.ProjectNames[i]}/{BuildContext.ProjectNames[i]}/modinfo.json", $"../../bin/ZipStaging/{context.Names[i]}/modinfo.json");
+			context.Zip($"../../bin/ZipStaging/{context.Names[i]}", $"../../bin/Releases/{context.Names[i]}_{context.Versions[i]}.zip");
+		}
+		DeleteDirectorySettings DDS = new()
+		{
+			Recursive = true
+		};
+		context.DeleteDirectory($"../../bin/ZipStaging/", DDS);
+		context.DeleteDirectory($"../../bin/Release", DDS);
+	}
 }
 
 [TaskName("Default")]
